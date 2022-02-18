2eeebca94e9c26e8e98b67caa1db1174ad70e8cb
Daniel McDonald <danielmcdonald@ucsd.edu>
2022-02-18 11:54:10 -0800

    this thing does stuff

--- q2_nc/_example.py
+++ q2_nc/_example.py
@@ -5,3 +5,10 @@ def samples_of_interest(metadata: pd.DataFrame) -> pd.Series:
     column = 'host_tax_id'
     value = '9606'
     return (metadata[column] == value)
+
+
+def pivot_classification(labels: pd.Series) -> pd.DataFrame:
+    df = pd.DataFrame(labels).reset_index()
+    pivot = df.pivot(index='sample-id', columns='label',
+                     values='label')
+    return ~(pivot.isnull())
