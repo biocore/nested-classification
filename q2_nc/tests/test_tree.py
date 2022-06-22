0e04ac6eb23e6d37cc20a092431a138bd8d1f3ef
jjjoelle <jfaybish@ucsd.edu>
2022-06-22 16:37:18 -0700

    tests.tsv + test_tree WIP

--- q2_nc/tests/test_tree.py
+++ q2_nc/tests/test_tree.py
@@ -6,19 +6,30 @@ from q2_nc.treeClass import TreeClass
 class TreeTests(TestCase):
 
     def setUp(self):
+        #homo tree goes 9605->(1425170,9606->(63221,741158))
+        #9605: 0
+        #1425170: 1 
+        #9606: 5
+        #63221: 5
+        #741158: 3
+        
+
         df = pd.read_csv('tests.tsv', sep='\t') 
-        homo_tree = "homo"
+        print(df)
+        homo_tree = 9605
         self.ncbi_tree = TreeClass(homo_tree, df, 'ncbi_taxon_id')
-        self.ncbi_tree.build_sample_tree()
+        
 
+    def test_sum_look_up_taxid(self):
+        real_sum = 14
+        trial_sum = self.ncbi_tree.look_up_taxid(9605)
+        self.assertEqual(real_sum, trial_sum)
 
-    
-    #build tree correctly
-    def test_print_all_samples(self):
-        self.fail()
-
-    def test_total_samples(self):
-        self.fail()
+    def test_sum_look_up_taxid(self):
+        real_sum = 14
+        root_node = self.ncbi_tree.get_tree.get_tree_root()
+        trial_sum = self.ncbi_tree.get_samples(root_node)
+        self.assertEqual(real_sum, trial_sum)
 
     def test_taxids_per_sample(self):
         self.fail()
